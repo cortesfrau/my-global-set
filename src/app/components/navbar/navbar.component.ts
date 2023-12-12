98e5abb5c8d092b4bf5421c62a6608bbf15d7f0a
Lluís Cortès Frau <lluiscortes@uoc.edu>
2023-12-12 02:37:25 +0100

    Minor changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthStateService } from 'src/app/services/auth-state.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+
+  public loggedIn: boolean | undefined;
+
+  constructor(
+    private AuthState: AuthStateService,
+    private Auth: AuthService,
+    private Router: Router
+  ){ }
+
+  ngOnInit() {
+
+    this.AuthState.authStatus.subscribe(value => this.loggedIn = value);
+  }
+
+  logout(): void {
+    this.Auth.logout();
+    this.Router.navigateByUrl('');
+  }
 
 }
